d8ee552147087423d4767384d2c74bf6c4b44bd8
smillbom <chanon.sing@bumail.net>
2017-09-27 20:29:28 +0700

    Update chapter-01.php

--- php/example/chapter-01.php
+++ php/example/chapter-01.php
@@ -2,8 +2,8 @@
 
 include ('line-bot-api/php/line-bot.php');
 
-$channelSecret = '551ec4feee0.....43cff0';
-$access_token  = '2og9ogezC8k.......5ZUEQQdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '3ac2c19071bc3293b0ef7918b3b51ec5';
+$access_token  = 'OPGo8kMpz7UlLcuayLL9xkIaWu8GnkDIS9sUHRj7h5dpSfeuIaW0muOvw5zBcG/1AIQkVRVJxKYIIPujGMaV2xcdavFKcPICfpAcAORw2By03jUo+bdETZKpg7i2TTzyXPGY92fsfp8FPFokeA+cYAdB04t89/1O/w1cDnyilFU=';
 
 $bot = new BOT_API($channelSecret, $access_token);
 	
