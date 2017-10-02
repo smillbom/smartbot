d9e55a9e991c7261f56c6fae60a813c26384d4dc
smillbom <chanon.sing@bumail.net>
2017-10-02 13:49:15 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -17,15 +17,13 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages = [				
-				{
-				   	'type' => 'text',
-					'text' => $text
-				},
-				{
-				  	'type' => 'text',
-					'text' => '12445'
-				}
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			],
+			[
+				'type' => 'text',
+				'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
