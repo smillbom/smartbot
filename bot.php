46fe6289d48c7a35feb2d554284c99ac848d73e0
smillbom <chanon.sing@bumail.net>
2017-10-02 15:46:05 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -14,9 +14,8 @@ $_msg = $arrJson['events'][0]['message']['text'];
  
  
 $api_key="<MLAB APIKEY>";
-$url = 'https://api.mlab.com/api/1/databases/edo_bot/collections/linebot?apiKey='.$api_key.'';
-$json = file_get_contents('https://api.mlab.com/api/1/databases/edo_bot/collections/linebot?apiKey='.$api_key.'&q={"question":"'.$_msg.'"}');
-$data = json_decode($json);
+$url = 'https://api.mlab.com/api/1/databases/duckduck/collections/linebot?apiKey='.$api_key.'';
+$json = file_get_contents('https://api.mlab.com/api/1/databases/duckduck/collections/linebot?apiKey='.$api_key.'&q={"question":"'.$_msg.'"}');$data = json_decode($json);
 $isData=sizeof($data);
  
 if (strpos($_msg, 'สอนเป็ด') !== false) {
