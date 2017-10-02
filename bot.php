ea1a05521a93cee729fcd2337c02d82d41551150
smillbom <chanon.sing@bumail.net>
2017-10-02 13:55:59 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -21,6 +21,11 @@ if (!is_null($events['events'])) {
 				'type' => 'text',
 				'text' => $text
 			];
+			
+			$messages = [
+				'type' => 'text',
+				'text' => 'fghgfh'
+			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
