fef6477a39adf31466e4ab2400caeced22bc838b
smillbom <chanon.sing@bumail.net>
2017-10-03 12:00:26 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -58,7 +58,7 @@ if (strpos($_msg, 'edo') !== false) {
     $arrPostData = array();
     $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
     $arrPostData['messages'][0]['type'] = "text";
-    $arrPostData['messages'][0]['text'] = $content ;
+    $arrPostData['messages'][0]['text'] = $_msg  ;
   }
 }
  
