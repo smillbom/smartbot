e9727b9e9a2f3fb14f4a00bed9cf668169fd2f48
smillbom <chanon.sing@bumail.net>
2017-10-03 23:09:07 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -84,7 +84,7 @@ if (strpos($_msg, 'edo') !== false) {
     $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
     $arrPostData['messages'][0]['type'] = "text";
     $arrPostData['messages'][0]['text'] = 'Key ของคุณถูกเปิดใช้ การโต้ตอบของผมต่อไปนี้มาจากผู้สร้างผมเพียงคนเดียว และคุณสามารถออกจากระบบได้เพียงพิมพ์คำว่า ออกอีโด้';
-}else if($_msg == $key){
+}else if($_msg == $key && $idchk != '' ){
     $arrPostData = array();
     $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
     $arrPostData['messages'][0]['type'] = "text";
