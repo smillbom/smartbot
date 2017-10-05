928fc75dd3c2155c17dba07477a23592c4d365c0
smillbom <chanon.sing@bumail.net>
2017-10-05 16:06:40 +0700

    fgh

--- bot.php
+++ bot.php
@@ -156,7 +156,6 @@ else if($isDatachk >0)
 			$arrPostData['messages'][0]['text'] = 'ขอบคุณที่บอก edo';
 		}
 	}
-
  //////////////////////////////////////////////////////add key by private
 	else if ($_msg == 'addkey') {
 		$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -186,27 +185,6 @@ else if($isDatachk >0)
 		$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
 		$arrPostData['messages'][0]['type'] = "text";
 		$arrPostData['messages'][0]['text'] = $randomString;  
-	}
-	else if ($_msg == 'key?'){
-		for($datakry_show as $rec){	
-			$arrPostData = array();
-			$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-			$arrPostData['messages'][0]['type'] = "text";
-			$arrPostData['messages'][0]['text'] = $rec->key; 
-
-			$channel = curl_init();
-			curl_setopt($channel, CURLOPT_URL,$strUrl);
-			curl_setopt($channel, CURLOPT_HEADER, false);
-			curl_setopt($channel, CURLOPT_POST, true);
-			curl_setopt($channel, CURLOPT_HTTPHEADER, $arrHeader);
-			curl_setopt($channel, CURLOPT_POSTFIELDS, json_encode($arrPostData));
-			curl_setopt($channel, CURLOPT_RETURNTRANSFER,true);
-			curl_setopt($channel, CURLOPT_SSL_VERIFYPEER, false);
-			$result = curl_exec($channel);
-			curl_close ($channel);
-
-		}
-
 	}
 ///////////////////////////////////////////////////event private
 	else
@@ -276,10 +254,10 @@ else{
 	}
 }   
 
-  //   $arrPostData = array();
-  //  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  // $arrPostData['messages'][0]['type'] = "text";
-  // $arrPostData['messages'][0]['text'] = $datakry_msg ;
+//     $arrPostData = array();
+//    $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+//   $arrPostData['messages'][0]['type'] = "text";
+//   $arrPostData['messages'][0]['text'] = $key_msg ;
 
 $channel = curl_init();
 curl_setopt($channel, CURLOPT_URL,$strUrl);
